d80c87d5ed16ce955a97c917970d57aca5136b52
sasikiran <sasikiranessamaraju@gmail.com>
2022-04-07 10:58:37 +0530

    print the longest common subsequence.

--- lab10/LCS.cpp
+++ lab10/LCS.cpp
@@ -11,6 +11,26 @@ public:
 		dp.assign(m + 1, vector<int>(n + 1, -1));
 		return lcs(a, b, m, n);
 	}
+
+	string getLCS(string a, string b) {
+		string ans;
+		int m = a.size();
+		int n = b.size();
+		int i = m, j = n;
+		while (i > 0 and j > 0) {
+			if (a[i - 1] == b[j - 1]) {
+				ans.push_back(a[i - 1]);
+				i--;
+				j--;
+			}
+			else if (dp[i - 1][j] > dp[i][j - 1]) {
+				i--;
+			}
+			else j--;
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
+	}
 private:
 	int lcs(string &a, string &b, int m, int n) {
 		if (m == 0 or n == 0) return dp[m][n] = 0;
@@ -25,5 +45,6 @@ int main() {
 	string b = "ace";
 	Solution algo;
 	cout << algo.lcs(a, b) << endl;
+	cout << algo.getLCS(a, b) << endl;
 	return 0;
 }
